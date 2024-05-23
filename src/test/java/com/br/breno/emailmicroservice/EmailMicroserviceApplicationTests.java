f1c9b07761fcc5a5e6bf85682ce70983aa28c537
BrenoM097 <melobreno89@gmail.com>
2024-05-23 15:04:09 -0300

    test

---
+++ src/test/java/com/br/breno/emailmicroservice/EmailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.breno.emailmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
