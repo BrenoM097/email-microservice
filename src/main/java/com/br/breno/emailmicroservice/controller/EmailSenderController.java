7ad061733ea455504745672bf94a199665a617fe
BrenoM097 <melobreno89@gmail.com>
2024-05-27 10:55:28 -0300

    Criação de documentação com o Swagger, springDoc

--- src/main/java/com/br/breno/emailmicroservice/controller/EmailSenderController.java
+++ src/main/java/com/br/breno/emailmicroservice/controller/EmailSenderController.java
@@ -3,9 +3,15 @@ package com.br.breno.emailmicroservice.controller;
 import com.br.breno.emailmicroservice.application.EmailSenderService;
 import com.br.breno.emailmicroservice.core.EmailRequest;
 import com.br.breno.emailmicroservice.core.exceptions.EmailServiceException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +25,11 @@ public class EmailSenderController {
     }
 
     @PostMapping("send")
+    @Operation(summary = "Send an email", description = "Send an email to a specified recipient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Email sent successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public ResponseEntity<String> emailSenderSes(@RequestBody EmailRequest request, @RequestParam String provider) {
         try {
             emailSenderService.sendEmail(request.to(), request.subject(), request.body(), provider);
--- src/main/java/com/br/breno/emailmicroservice/core/EmailRequest.java
+++ src/main/java/com/br/breno/emailmicroservice/core/EmailRequest.java
@@ -1,3 +1,6 @@
 package com.br.breno.emailmicroservice.core;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Email request model")
 public record EmailRequest(String to, String subject, String body) {}
